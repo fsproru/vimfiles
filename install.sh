6a587fe723c36a2c37fae02f56d8b38929a3cd43
Alexander Tamoykin <at@zestcash.com>
2012-10-09 02:13:42 -1000

    Tell a user what is happening during the installation

--- install.sh
+++ install.sh
@@ -3,9 +3,17 @@
 if [[ -e $HOME/.vimrc || -e $HOME/.vim ]]; then
   echo "Ooops: ~/.vimrc or ~/.vim already exists. \n\
        It looks like you already have some vim configuration. \n\
-       Delete .vim and .vimrc and run install.sh again."
+       Try to run ./uninstall.sh"
 else
+  echo === Checking out vundle
+  git submodule update --init
+
+  echo === Linking vim directories
   ln -s $PWD/vimrc $HOME/.vimrc
   ln -s $PWD $HOME/.vim
+
+  echo === Vundling
   vim +BundleInstall +qall
+
+  echo === Done
 fi
