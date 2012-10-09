c0707a1ef9cbd029106db4f84a81a488b631550d
Alexander Tamoykin <at@zestcash.com>
2012-10-09 01:58:40 -1000

    uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [[ -e $HOME/.vimrc || -e $HOME/.vim ]]; then
+
+  # asking for confirmation
+  while true
+  do
+    echo "I'll delete $HOME/.vim and $HOME/.vimrc\n\
+Hope you don't have anything important there.\n\n\
+Sounds good? (yes/no)"
+
+    read CONFIRM
+    case $CONFIRM in
+      y|Y|YES|yes|Yes) break ;;
+      n|N|no|NO|No)
+        echo Aborting - you entered $CONFIRM
+        exit
+        ;;
+      *) echo Please type yes or no
+    esac
+  done
+
+  # deleting
+  echo "You entered $CONFIRM. So I'm deleting it."
+  rm ~/.vim
+  rm ~/.vimrc
+else
+  echo "vimfiles are already deleted. You're good."
+fi
